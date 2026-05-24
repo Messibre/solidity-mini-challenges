a963055b05a5767b8666ff061328a890782c21c2
Messibre <messibre27@gmail.com>
2026-05-24 16:39:22 +0300

    deploy all four payFarma contracts

--- payFarma/CooperativeTreasury.sol
+++ payFarma/CooperativeTreasury.sol
@@ -6,15 +6,36 @@ import {CoffeeDelivery} from "./CoffeeDelivery.sol";
 
 contract CooperativeTreasury is FarmerRegistry,CoffeeDelivery{
 
+    address public loanManager;
     uint256 public totalETHReceived;
+    //eth that will be distributed
+    uint256 public distributionPool;
+    //eth saved for loans and repaiments
+    uint256 public loanReserve;
+
     mapping(address => uint256) public pendingPayouts;
+    uint256 constant public distributionPercent = 70;
 
     event ethRecieved(uint256 amount , address from);
     event payoutDistributed();
     event paidFarmer(address farmer, uint256 amount);
+    event repaymentReceived(address indexed farmer, uint256 amount); 
 
-    constructor(address _contractAddress) FarmerRegistry() CoffeeDelivery(_contractAddress) {}
 
+    constructor(address _contractAddress ) FarmerRegistry() CoffeeDelivery(_contractAddress) {
+        
+    }
+
+    modifier onlyLoanManager(){
+        require(msg.sender == loanManager,"only loan contract can call this");
+        _;
+
+    }
+    function setLoanManager(address _loanManager) external onlyOwner {
+        require(loanManager == address(0), "Loan manager already set");
+        require(_loanManager != address(0), "Invalid address");
+        loanManager = _loanManager;
+    }
 
     function distributeShares(uint256 amountToBeDistributed) internal {
         uint256 totalUnpaid;
@@ -35,21 +56,50 @@ contract CooperativeTreasury is FarmerRegistry,CoffeeDelivery{
     }
 
     receive() external payable{
+
         emit ethRecieved(msg.value, msg.sender);
+
         totalETHReceived += msg.value;
-        distributeShares(msg.value);
+
+        uint256 finalDistribution = (distributionPercent * msg.value ) / 100;
+
+        distributionPool += finalDistribution;
+        loanReserve += (msg.value - finalDistribution);
+
+        distributeShares(finalDistribution);
         emit payoutDistributed();
+    }
 
-
+     function receiveRepayment() external payable onlyLoanManager {
+        loanReserve += msg.value;
     }
 
     function withdraw() public {
-        require(pendingPayouts[msg.sender] > 0, "no pending amount:(");
-        (bool success,)= payable(msg.sender).call{value:pendingPayouts[msg.sender] }("");
-        require(success, "payout failed!");
-        emit paidFarmer(msg.sender, pendingPayouts[msg.sender]);
+        uint256 amount = pendingPayouts[msg.sender];
+        require(amount > 0, "no pending amount:(");
+
+        distributionPool -= amount;
         pendingPayouts[msg.sender] =0;
 
 
+        (bool success,)= payable(msg.sender).call{value:amount }("");
+        require(success, "payout failed!");
+
+        emit paidFarmer(msg.sender, amount);
+
+
     }
+
+    function getLoan(address farmer , uint256 amount) public onlyLoanManager {
+        require(amount >0 , "invalid amount");
+        require(amount <= loanReserve, "Insufficient funds in loan reserve"); 
+
+        loanReserve -= amount;
+
+        (bool success,)= payable(farmer).call{value:amount }("");
+        require(success, "loan payout failed!");
+
+    }
+
+    
 }
--- payFarma/MicroLoanManager.sol
+++ payFarma/MicroLoanManager.sol
@@ -12,6 +12,11 @@ interface  IFarmerRegistry {
     function getFarmer(address _farmerAddress) external view returns(Farmer memory );    
 }
 
+interface ICooperativeTreasury {
+       function getLoan(address farmer , uint256 amount) external;
+        function receiveRepayment() external payable; 
+}
+
 contract MicroLoanManager{
 
     enum Status {
@@ -30,27 +35,33 @@ contract MicroLoanManager{
         Status loanStatus;
     }
     
-    address owner;
-    IFarmerRegistry farmerRegis;
+    address public owner;
+    IFarmerRegistry public farmerRegis;
+    ICooperativeTreasury public treasury;
+
     mapping(address => uint256[]) public myLoans;
     Loan[] public allLoans;
+
     address public treasuryAddress;
 
     event loanRequested(address farmer, uint256 amount);
     event loanApproved(uint256 loanId);
     event loanRejected(uint256 loanId);
-    event loanPaid(uint256 loanId);
+    event loanPaid(uint256 loanId , uint256 amount);
 
 
-    constructor(address _registryAddress){
-        owner = msg.sender;
-       farmerRegis = IFarmerRegistry(_registryAddress);
-
-    }
+   
 
     modifier onlyOwner{
         require(msg.sender ==owner, "only owner can do this");
         _;
+    }
+
+     constructor(address _registryAndtreasuryAddress){
+        owner = msg.sender;
+       farmerRegis = IFarmerRegistry(_registryAndtreasuryAddress);
+       treasury = ICooperativeTreasury(_registryAndtreasuryAddress);
+
     }
 
    function requestLoan(uint amount) public {
@@ -58,8 +69,10 @@ contract MicroLoanManager{
         require(checkedFarmer.active, "Farmer is not active or registered"); 
         require(amount >0 ,"invalid amount");
         
-        Loan memory l1 = Loan(allLoans.length + 1,msg.sender , amount ,0 ,Status.pending);
-        myLoans[msg.sender].push(allLoans.length +1 );
+        uint256 newLoanId = allLoans.length + 1;
+        Loan memory l1 = Loan(newLoanId, msg.sender, amount, 0, Status.pending);
+
+        myLoans[msg.sender].push(newLoanId);
         allLoans.push(l1);
 
         emit loanRequested(msg.sender, amount);
@@ -67,13 +80,36 @@ contract MicroLoanManager{
    }
 
    function approveLoan(address farmer, uint256 _loanId) public onlyOwner{
-       require(_loanId > allLoans.length,"loan index out ofbound");
+       require(_loanId > 0 && _loanId <= allLoans.length,"loan index out ofbound");
        require(myLoans[farmer].length >0,"farmer has no registered loans");
 
         Loan storage checkLoan = allLoans[_loanId - 1];
+        require(checkLoan.loanStatus == Status.pending, "Loan not pending");
+
         checkLoan.loanStatus= Status.approved;
 
+        treasury.getLoan(farmer, checkLoan.requestedAmount);
         emit loanApproved(_loanId);
         
+   }
+
+   function repayLoan(uint256 _loanId) public payable{
+        require(_loanId > 0 && _loanId <= allLoans.length,"loan index out ofbound");
+
+        Loan storage checkLoan = allLoans[_loanId - 1];
+        require(checkLoan.farmer == msg.sender, "Not your loan to repay");
+        require(checkLoan.loanStatus == Status.approved, "Loan not pending");
+        require(msg.value > 0, "Must send ETH to repay");
+
+        checkLoan.paidAmount += msg.value;
+
+         if (checkLoan.paidAmount >= checkLoan.requestedAmount) {
+            checkLoan.loanStatus = Status.repaid;
+        }
+
+        treasury.receiveRepayment{value: msg.value}();
+
+        emit loanPaid(_loanId, msg.value);
+
    }
 }
